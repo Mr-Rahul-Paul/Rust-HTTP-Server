c2fbe0d78bd178c8c2086e691a195e960ffa8304
Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-07-11 03:00:54 +0530

    added a health api to check status

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Json, routing::get};
+use serde_json::json;
+use serde_json::{Result, Value};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+async fn health_check() -> Json<Value> {
+    Json(json!({
+     "status": "Server is up and running",
+     "timestamp": chrono::Utc::now().to_rfc3339()
+    }))
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, World!");
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/health", get(health_check));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("{}", addr);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Server running on http://{}", addr);
+    axum::serve(listener, app).await.unwrap();
 }
